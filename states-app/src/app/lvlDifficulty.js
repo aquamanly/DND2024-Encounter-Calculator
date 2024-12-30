73147a1ac243ee5c482e2e73417b8903247e188d
aquamanly <aharnold@gmail.com>
2024-12-30 14:22:41 -0600

    Create lvlDifficulty.js

---
+++ states-app/src/app/lvlDifficulty.js
@@ -0,0 +1,23 @@
+const xpBudgetPerCharacter = [
+  { level: 1, low: 50, moderate: 75, high: 100 },
+  { level: 2, low: 100, moderate: 150, high: 200 },
+  { level: 3, low: 150, moderate: 225, high: 400 },
+  { level: 4, low: 250, moderate: 375, high: 500 },
+  { level: 5, low: 500, moderate: 750, high: 1100 },
+  { level: 6, low: 600, moderate: 1000, high: 1400 },
+  { level: 7, low: 750, moderate: 1300, high: 1700 },
+  { level: 8, low: 1000, moderate: 1700, high: 2100 },
+  { level: 9, low: 1300, moderate: 2000, high: 2600 },
+  { level: 10, low: 1600, moderate: 2300, high: 3100 },
+  { level: 11, low: 1900, moderate: 2900, high: 4100 },
+  { level: 12, low: 2200, moderate: 3700, high: 4700 },
+  { level: 13, low: 2600, moderate: 4200, high: 5400 },
+  { level: 14, low: 2900, moderate: 4900, high: 6200 },
+  { level: 15, low: 3300, moderate: 5400, high: 7800 },
+  { level: 16, low: 3800, moderate: 6100, high: 9800 },
+  { level: 17, low: 4500, moderate: 7200, high: 11700 },
+  { level: 18, low: 5000, moderate: 8700, high: 14200 },
+  { level: 19, low: 5500, moderate: 10700, high: 17200 },
+  { level: 20, low: 6400, moderate: 13200, high: 22000 },
+];
+export default xpBudgetPerCharacter;
